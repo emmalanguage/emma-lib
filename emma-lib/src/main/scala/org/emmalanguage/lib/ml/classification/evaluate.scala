54ea8f31a7dc1fe230ba9129ce995672fe68eb59
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2017-07-05 21:41:42 +0200

    fixup! [ML] Added `ml.classification.eval` module.
    - Syntactic improvements.

--- emma-lib/src/main/scala/org/emmalanguage/lib/ml/classification/evaluate.scala
+++ emma-lib/src/main/scala/org/emmalanguage/lib/ml/classification/evaluate.scala
@@ -23,38 +23,37 @@ import lib.ml._
 
 @emma.lib
 object evaluate {
+  type Hypothesis = DVector => Boolean
+  type TestBag[ID] = DataBag[LDPoint[ID, Boolean]]
+
   val TP = 0
   val FP = 1
   val FN = 2
   val TN = 3
 
-  type Hypothesis = DVector => Boolean
-  type TestBag[ID] = DataBag[LDPoint[ID, Boolean]]
-
   def apply[ID: Meta](h: Hypothesis)(xs: TestBag[ID]): DataBag[LDPoint[ID, Int]] =
-    xs.map { x => x.copy(label = {
-      val act = x.label
-      val pred = h(x.pos)
-      (if (pred) 0 else 2) | (if (act) 0 else 1)
-    })}
+    for (x <- xs) yield {
+      val quadrant = (if (h(x.pos)) 0 else 2) | (if (x.label) 0 else 1)
+      LDPoint(x.id, x.pos, quadrant)
+    }
 
   def precision[ID: Meta](h: Hypothesis)(xs: TestBag[ID]): Double = {
-    val evaluated = evaluate(h)(xs)
-    val tp = evaluated.count(_.label == TP).toDouble
-    val fp = evaluated.count(_.label == FP).toDouble
+    val es = evaluate(h)(xs)
+    val tp = es.count(_.label == TP).toDouble
+    val fp = es.count(_.label == FP).toDouble
     tp / (tp + fp)
   }
 
   def recall[ID: Meta](h: Hypothesis)(xs: TestBag[ID]): Double = {
-    val evaluated = evaluate(h)(xs)
-    val tp = evaluated.count(_.label == TP).toDouble
-    val fn = evaluated.count(_.label == FN).toDouble
+    val es = evaluate(h)(xs)
+    val tp = es.count(_.label == TP).toDouble
+    val fn = es.count(_.label == FN).toDouble
     tp / (tp + fn)
   }
 
   def f1score[ID: Meta](h: Hypothesis)(xs: TestBag[ID]): Double = {
-    val prec = precision(h)(xs)
-    val rec = recall(h)(xs)
-    2.0 * ((prec * rec) / (prec + rec))
+    val p = precision(h)(xs)
+    val r = recall(h)(xs)
+    2.0 * (p * r) / (p + r)
   }
 }
